277ac4670b7d80f6914c3104f80ab4d100846afc
Mora <morafidela1@gmail.com>
2024-08-31 23:35:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Profil from "./Profil";
+
+export default function App() {
+  return (
+    <section>
+      <h1 className="h1">Pahlawan Indonesia</h1>
+      <div>
+      <Profil/>
+      </div>
+    </section>
+  );
+}
---
+++ src/Profil.jsx
@@ -0,0 +1,42 @@
+export default function Profil() {
+    return (
+        <div className="container">
+            <div className="pict">
+                <img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1629103956/ruro8lifdnazz6rfnlhn.jpg" alt=""/>
+                <h2>Ir. Soekarno</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/f8882995-2b29-43e4-a668-563ef3fb0483.jpg" alt=""/>
+                <h2>R.A Kartini</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/d4779b72-5ece-49b5-8206-e714e532998c.png" alt=""/>
+                <h2>Raden Dewi Sartika</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/1c4146c7-1ad2-4dbb-ab5e-10afb2f768fe.png" alt=""/>
+                <h2>Pangeran Diponegoro</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/7da8e2ac-59dd-42f8-ab75-a640ca06523f.png" alt=""/>
+                <h2>Teuku Umar</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/d150b792-a4f0-48ec-b7b0-6e3e9e81f4df.png" alt=""/>
+                <h2>Cut Nyak Dien</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/d2f5095d-3898-4e1c-ad44-abe98b4846ce.png" alt=""/>
+                <h2>Jenderal Ahmad Yani</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/1b1b56c3-6323-4067-b15e-1797845b5dca.png" alt=""/>
+                <h2>Tjut Meutia</h2>
+            </div>
+            <div className="pict">
+                <img src="https://cdn-web-2.ruangguru.com/landing-pages/assets/2629d690-4287-4040-b747-f82faf2c5b48.png" alt=""/>
+                <h2>Tuanku Imam Bonjol</h2>
+            </div>
+      </div>
+    );
+  }
